a165f07926c6a8ea0f93fce5753faa11ad715fdf
alailsonko <aalailson3@gmail.com>
2021-01-06 14:50:58 -0300

    feat(user-api): implements usermodel struct

---
+++ user-api/models/user_model.go
@@ -0,0 +1,12 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+// UserModel - struct
+type UserModel struct {
+	gorm.Model
+	Username    string `gorm:"not null" json:"username"`
+	Cpf         string `gorm:"not null" json:"cpf"`
+	Email       string `gorm:"not null" json:"email"`
+	PhoneNumber string `gorm:"not null" json:"phone_number"`
+}
