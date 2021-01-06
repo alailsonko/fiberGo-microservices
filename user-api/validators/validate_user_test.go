e72f1eb9758bddff78b521886ca437e7cefd7c35
alailsonko <aalailson3@gmail.com>
2021-01-05 22:56:35 -0300

    test(user-api): implements username to be required

---
+++ user-api/validators/validate_user_test.go
@@ -0,0 +1,40 @@
+package validators
+
+import (
+	"fmt"
+	"testing"
+)
+
+type error interface {
+	Error() string
+}
+
+func TestValidateUser(t *testing.T) {
+
+	// should return error if name is not provided
+	ut := new(User)
+
+	ut.Username = "alailson"
+
+	err := ut.ValidateUser()
+
+	expected := "username: cannot be blank."
+
+	if err != nil && err.Error() == expected {
+		fmt.Println(err.Error(), expected)
+		t.Errorf("should return error if name is not provided: %v", err)
+	} else if err == nil {
+		fmt.Println(err, expected)
+		t.Log("passing: should return error if name is not provided.")
+	}
+}
+
+// name cpf email phone_number
+
+// should return error if cpf is not provided
+// should return error if cpf min11-max11 is not provided
+// should return error if email is not provided
+// should return error if email is not invalid
+// should return error if phone_number is not provided
+// should return error if phone_number min11-max11 is not provided
+// should return nil if all data is valid
