786dd39ce9a6bab9e4468197ed402f3d1d0d96f7
alailsonko <aalailson3@gmail.com>
2021-01-05 22:55:33 -0300

    chore(user-api): add type for user

---
+++ user-api/usecases/user_type.go
@@ -0,0 +1,10 @@
+package usecases
+// name cpf email phone_number
+
+// UserType - type for user
+type UserType struct {
+	Username string `json:"username"`
+	Cpf string `json:"cpf"`
+	Email string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+} 
