2d314a9c66152864517862b1eb6d75d76fbbeb6b
alailsonko <aalailson3@gmail.com>
2021-01-06 05:55:47 -0300

    feat(user-api): implements email validation, required and valid email

--- user-api/validators/validate_user.go
+++ user-api/validators/validate_user.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"user-api.com/usecases"
 )
 
@@ -19,6 +20,7 @@ func (ut User) ValidateUser() error {
 	return validation.ValidateStruct(&ut,
 		validation.Field(&ut.Username, validation.Required),
 		validation.Field(&ut.Cpf, validation.Required, validation.Length(11, 11)),
+		validation.Field(&ut.Email, validation.Required, is.Email),
 	)
 }
 
