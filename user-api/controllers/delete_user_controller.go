82d226a53ced78df3ecf0300551469280c9fc9bb
alailsonko <aalailson3@gmail.com>
2021-01-08 00:18:48 -0300

    feat(user-api): implements delete user by id controller

---
+++ user-api/controllers/delete_user_controller.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"user-api.com/database"
+	"user-api.com/models"
+)
+
+func DELETEUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DB
+
+	var user models.User
+
+	db.First(&user, id)
+
+	if user.ID == 0 {
+		c.JSON(fiber.Map{"message": "not found"})
+		c.Status(404)
+		return nil
+	}
+	db.Delete(&user)
+
+	c.JSON(fiber.Map{"message": "delete successfully"})
+	c.Status(200)
+
+	return nil
+}
