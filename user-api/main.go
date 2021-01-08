3835e706c7d0ddfcdd886ab29360343d6cd13ea1
alailsonko <aalailson3@gmail.com>
2021-01-07 22:07:16 -0300

    feat(user-api): connect database on main.go

--- user-api/main.go
+++ user-api/main.go
@@ -4,13 +4,16 @@ import (
 	"log"
 	"os"
 
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"user-api.com/database"
 	"user-api.com/routes"
 	"user-api.com/setup_app"
 )
 
 func main() {
-	app := setup_app.SetupApp()
 
+	app := setup_app.SetupApp()
+	database.ConnectPGDB()
 	routes.UserSetupRoutes(app)
 
 	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
