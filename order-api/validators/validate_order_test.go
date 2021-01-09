4804eb4cc2ea4a08379e7d50d215af757d39caee
alailsonko <aalailson3@gmail.com>
2021-01-09 02:52:20 -0300

    test(order-api): error case item_description min5-max 200 is not provided

--- order-api/validators/validate_order_test.go
+++ order-api/validators/validate_order_test.go
@@ -23,7 +23,34 @@ func TestValidateOrder(t *testing.T) {
 		t.Log("passing: should return error if user_id is not provided.")
 	}
 	// should return error case item_description is not provided
-	ot.ItemDescription = "valid description"
+	ot.ItemDescription = `v
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	i`
 
 	err = ot.ValidateOrder()
 
@@ -36,10 +63,51 @@ func TestValidateOrder(t *testing.T) {
 		fmt.Println(err, expected)
 		t.Log("passing: should return error if item_description is not provided.")
 	}
+	// should return error case item_description min5-max 200 is not provided
+	ot.ItemDescription = `v
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+	fdsssssssssssssssssssssssss
+
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	fsfsdfsd
+	i`
+
+	err = ot.ValidateOrder()
+
+	expected = "item_description: error case item_description min5-max 200 is not provided."
+
+	if err != nil && err.Error() == expected {
+		fmt.Println(err.Error(), expected)
+		t.Errorf("error case item_description min5-max 200 is not provided: %v", err)
+	} else if err == nil {
+		fmt.Println(err, expected)
+		t.Log("passing: error case item_description min5-max 200 is not provided.")
+	}
+	// should return error case item_quantity is not provided
+
 }
 
-// should return error case item_description min5-max 200 is not provided
-// should return error case item_quantity is not provided
 // should return error case item quantity is not float
 // should return error case item_price is not provided
 // should return error case item_price is not float
