5ff0ef539b010cdd482fdb659b0b85a40a1f66e6
alailsonko <aalailson3@gmail.com>
2021-01-09 06:25:12 -0300

    feat(order-api): implements routes on main.go

--- order-api/main.go
+++ order-api/main.go
@@ -6,14 +6,15 @@ import (
 
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"order-api.com/database"
+	"order-api.com/routes"
 	"order-api.com/setup_app"
 )
 
 func main() {
 
 	app := setup_app.SetupApp()
-
 	database.ConnectPGDB()
+	routes.OrderSetupRoutes(app)
 
 	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
