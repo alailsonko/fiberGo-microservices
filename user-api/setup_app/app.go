93b1749ebe4f894dfe042e906e4ce1c1b2619a3e
alailsonko <aalailson3@gmail.com>
2021-01-09 20:22:58 -0300

    feat(user-api): implements cors

--- user-api/setup_app/app.go
+++ user-api/setup_app/app.go
@@ -2,14 +2,13 @@ package setup_app
 
 import (
 	fiber "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func SetupApp() *fiber.App {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world")
-	})
+	app.Use(cors.New())
 
 	return app
 }
