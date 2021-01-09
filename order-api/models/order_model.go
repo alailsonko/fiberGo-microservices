d18d230feea2b8558aeea8a81b5e127fa1d42b88
alailsonko <aalailson3@gmail.com>
2021-01-09 06:22:06 -0300

    fix(order-api): fix tag in models

--- order-api/models/order_model.go
+++ order-api/models/order_model.go
@@ -10,7 +10,7 @@ type Order struct {
 
 	UserID          string  `gorm:"not null" json:"user_id"`
 	ItemDescription string  `gorm:"not null" json:"item_description"`
-	ItemQuantity    float64 `gorm:"not null" json:"item_item_quantity"`
+	ItemQuantity    float64 `gorm:"not null" json:"item_quantity"`
 	ItemPrice       float64 `gorm:"not null" json:"item_price"`
 	TotalPrice      float64 `gorm:"not null" json:"total_price"`
 }
