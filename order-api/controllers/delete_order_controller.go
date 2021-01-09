d8b38eb44685bb66cda177d24395a1114e92ad0a
alailsonko <aalailson3@gmail.com>
2021-01-09 07:23:13 -0300

    feat(order-api): implements delete order by id controller

---
+++ order-api/controllers/delete_order_controller.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"order-api.com/database"
+	"order-api.com/models"
+)
+
+func DELETEOrder(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DB
+
+	var order models.Order
+
+	db.First(&order, id)
+
+	if order.ID == 0 {
+		c.JSON(fiber.JSON{"message": "not found"})
+		c.Status(404)
+		return nil
+	}
+
+	db.Delete(&order)
+
+	c.JSON(fiber.JSON{"message": "deleted successfully"})
+	c.Status(200)
+
+	return nil
+}
