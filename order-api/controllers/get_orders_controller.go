f327da8539180148333fa8667ff58effd7274d35
alailsonko <aalailson3@gmail.com>
2021-01-11 10:21:12 -0300

    feat(order-api): implements getuserscache logic in getsorders controller

--- order-api/controllers/get_orders_controller.go
+++ order-api/controllers/get_orders_controller.go
@@ -2,15 +2,20 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"order-api.com/database"
-	"order-api.com/models"
+	"order-api.com/cache"
 )
 
 func GETOrders(c *fiber.Ctx) error {
-	db := database.DB
-	var orders []models.Order
+	var cc *cache.Cache
 
-	db.Find(&orders)
+	orders, err := cc.GetOrdersCache()
+
+	if err != nil {
+		c.JSON(fiber.Map{"error": err})
+		c.Status(fiber.StatusBadRequest)
+		return nil
+	}
+
 	c.JSON(fiber.Map{"data": orders})
 	c.Status(200)
 	return nil
